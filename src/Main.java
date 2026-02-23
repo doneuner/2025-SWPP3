a1e3e76f4dc57d3353ff9f95fc1aca9a174352f7
Dominik Neuner <do.neuner@tsn.at>
2026-02-23 13:24:46 +0100

    sout

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello, World! " + i);
         }
+
+        System.out.println("Goodbye, World!");
     }
 }
